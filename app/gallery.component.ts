6597a2294a66b8fe6a1d3333bfd20fbe53af97aa
nogalavi <noga.lavi1@gmail.com>
2016-10-18 20:55:00 +0300

    Added gallery.service that gets JSON from URL (now uses a mock JSON)

--- app/gallery.component.ts
+++ app/gallery.component.ts
@@ -4,6 +4,7 @@
 
 import {Component, Input} from 'angular2/core'
 import {PageComponent} from "./page.component";
+import {GalleryService} from "./gallery.service";
 
 @Component({
     selector: 'gallery',
@@ -18,8 +19,10 @@ import {PageComponent} from "./page.component";
 
     `,
     inputs: ['feedUrl', 'searchInput', 'paginationInput', 'resultsPerPageInput', 'sortingInput', 'rotateTimeInput'],
-    directives: [PageComponent]
+    directives: [PageComponent],
+    providers: [GalleryService]
 })
+
 export class GalleryComponent {
 
     searchInput = true;
@@ -27,25 +30,11 @@ export class GalleryComponent {
     resultsPerPageInput = 10;
     sortingInput = true;
     rotateTimeInput = 4;
-    feedUrl;
+    feed;
 
-    feed =  [
-
-    {   "title": "Blue river (much better in original size - press)",
-        "url": "http://farm9.static.flickr.com/8305/7893507666_0d25cd9f30.jpg",
-        "date": "Thu, 30 Aug 2012 10:41:00 -0400",
-        "id": 0
-    },
-
-    {   "title": "Dangerously beautiful paths",
-        "url": "http://farm8.static.flickr.com/7275/7550745422_3e323cd79e.jpg",
-        "date": "Thu, 12 Jul 2012 03:27:00 -0400",
-        "id": 1
+    constructor (galleryService: GalleryService) {
+        this.feed = galleryService.getFeed();
     }
-
-    ];
-
-
 
 
 
---
+++ app/gallery.service.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by nogalavi on 18/10/2016.
+ */
+
+export class GalleryService {
+    getFeed (): any {
+        return [
+
+            {   "title": "Blue river (much better in original size - press)",
+                "url": "http://farm9.static.flickr.com/8305/7893507666_0d25cd9f30.jpg",
+                "date": "Thu, 30 Aug 2012 10:41:00 -0400",
+                "id": 0
+            },
+
+            {   "title": "Dangerously beautiful paths",
+                "url": "http://farm8.static.flickr.com/7275/7550745422_3e323cd79e.jpg",
+                "date": "Thu, 12 Jul 2012 03:27:00 -0400",
+                "id": 1
+            }
+
+        ];
+
+
+    }
+}
