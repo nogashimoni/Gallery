2a1d88dd3d0333ca6e2db75cdfdcedf9959b747c
nogalavi <noga.lavi1@gmail.com>
2016-10-25 10:33:02 +0300

    Removed a redundent import in main.ts Final version

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import {enableProdMode} from "@angular/core";
-
-import { GalleryModule } from './app.module';
 import {AppModule} from "./app.module";
 
 enableProdMode();
